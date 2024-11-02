912e9494b0de6978cba6e3a33cbfd7216b7ad60f
John Ossai <johnossai20@gmail.com>
2024-11-02 20:13:51 +0100

    "Added animation to image and updated font styles in Home Display component"

--- src/components/Home/Display/index.tsx
+++ src/components/Home/Display/index.tsx
@@ -12,13 +12,14 @@ const Display = () => {
          <Image
             src={DisplayImage}
             alt="Display Image"
-            className="absolute top-0 left-0 h-full w-full object-cover transform scale-100 transition-transform duration-500 ease-in-out hover:scale-105"
+            className="absolute top-0 left-0 h-full w-full object-cover animate-slow-zoom"
          />
 
          {/* Text Content */}
          <div className="relative z-20 mx-auto max-w-[1000px] px-4 text-center">
-            <h1 className="text-white font-bold w-full text-[8vw] md:text-[4vw] lg:text-[3vw] xl:text-[2.5vw]">
-               We train YOU, upgrade YOU, and position YOU for a global digital revolution.
+            <h1 className="text-white font-medium w-full text-[8vw] md:text-[4vw] lg:text-[5vw] xl:text-[4vw]">
+               We train <span className="font-bold">YOU</span>, upgrade <span className="font-bold">YOU</span>, and
+               position <span className="font-bold">YOU</span> for a global digital revolution.
             </h1>
          </div>
       </div>
