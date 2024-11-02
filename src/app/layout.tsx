6ce0e41e09e6e4a88f5f21add25e4a302dbd6c40
John Ossai <johnossai20@gmail.com>
2024-11-02 21:19:13 +0100

    "Moved import statement for 'globals.css' to below component imports"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+
 import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Dropdown";
 
+import "./globals.css";
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
 //   variable: "--font-geist-sans",
